b763e1f29328d2cd21031fb0f4e9c1af59de89f2
Sean Flaherty <5176462+sean-flaherty@users.noreply.github.com>
2019-03-26 13:35:04 -0400

    Add Form Validation Recruit Registration
    Trello card 31

--- events/forms.py
+++ events/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from django.forms import inlineformset_factory
 from django.core.validators import RegexValidator
+from django.core.validators import validate_email
 from .models import Candidate, JobPosting, Attendance
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Submit, ButtonHolder, Fieldset, HTML, Button, Row, Field
 
 
 class RegisterForm(forms.Form):
-    candidate_first_name = forms.CharField(label='First name:', max_length=200)
-    candidate_last_name = forms.CharField(label='Last name:', max_length=200)
+    candidate_first_name = forms.CharField(label='First name:', max_length=50)
+    candidate_last_name = forms.CharField(label='Last name:', max_length=50)
     candidate_email = forms.EmailField(label='Email:', max_length=200)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{8,15}$',
-                                 message='Phone number must be entered in the format: ''+999999999''. Up to 15 digits allowed.')
-    candidate_phone = forms.CharField(validators=[phone_regex], label='Phone number:', max_length=15, required=False)
+    phone_regex = RegexValidator(regex=r'^\d{3}-\d{3}-\d{4}$',
+                                 message='Phone number must be entered in the format: \'123-123-1234\'.')
+    candidate_phone = forms.CharField(validators=[phone_regex], label='Phone number:', max_length=12, required=False)
     JobPostingFormSet = inlineformset_factory(JobPosting, Attendance, fields=('selected_job_posting',))
     candidate_job_postings = forms.MultipleChoiceField(label='Jobs:', initial='', widget=forms.CheckboxSelectMultiple(), required=True)
