fa059c373874a6f52d43cdfeb38bfe8b9bcadd56
Sean Flaherty <5176462+sean-flaherty@users.noreply.github.com>
2019-04-02 15:51:09 -0400

    base

--- events/urls.py
+++ events/urls.py
@@ -6,5 +6,6 @@ app_name = 'events'
 
 urlpatterns = [
     url(r'^(?P<event_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^edit/(?P<event_id>[0-9]+)?/?$', views.edit, name='edit'),
     url(r'^$', views.index, name='index'),
 ]
