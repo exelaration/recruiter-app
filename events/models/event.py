f64e97eb39129c9452c7d3b1a6ed2e8c3d42cc4d
Sean Flaherty <5176462+sean-flaherty@users.noreply.github.com>
2019-04-08 14:01:47 -0400

    Error on saving auto email without email address or template

--- events/models/event.py
+++ events/models/event.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 from events.models.job_posting import JobPosting
 from sendemail.models.email_template import EmailTemplate
@@ -20,6 +21,15 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+    def clean(self):
+        if self.auto_email:
+            if not self.auto_email_from:
+                raise ValidationError({'auto_email_from': ('Auto Email must have corresponding sender defined.')})
+            if not self.email_template:
+                raise ValidationError({'email_template': ('Auto Email must have corresponding template defined.')})
+
+        return super(Event, self)
 
     class Meta:
         ordering = ['-date_time']
