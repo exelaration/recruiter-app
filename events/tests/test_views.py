d01b5c5bfd41358c855bc6183831d05972060541
Catherine Stahl <46716216+c-stahl@users.noreply.github.com>
2019-04-09 17:25:36 -0400

    Test planning

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+# TODO: Test get request on /events or programmatically pulled URL: registration form is returned
+# TODO: Test post request on /events: Attendance is added
+# TODO:
 
 # TODO: Test that emails autosend when someone registers without logging in
+#   Check the most recent log against what was expected, or even better find
+#   a way to make sure the log is the right one for this registration attempt
