34a551442c23f976a399fa12ffe08f90ea8926c5
Catherine Stahl <46716216+c-stahl@users.noreply.github.com>
2019-04-10 12:02:20 -0400

    Start to outline tests

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -1,8 +1,30 @@
 from django.test import TestCase
 
+
 # TODO: Test get request on /events or programmatically pulled URL: registration form is returned
+class RegistrationFormTest(TestCase):
+    # def setUp(self):
+    #     pass
+
+    # TODO
+    def test_get_registration_form(self):
+        pass
+
+    # TODO: test anything that should be pre-populated in specific circumstances
+
+
 # TODO: Test post request on /events: Attendance is added
-# TODO:
+class RegistrationFormTest(TestCase):
+    # TODO
+    def test_post_valid_registration_form(self):
+        pass
+
+    # TODO: one or more per registration field - maybe in test_forms.py instead?
+    def test_registration_form_invalid_for_each_field(self):
+        pass
+
+# TODO: Test get_job_postings_for_event
+# TODO: Test update_or_create_candidate
 
 # TODO: Test that emails autosend when someone registers without logging in
 #   Check the most recent log against what was expected, or even better find
