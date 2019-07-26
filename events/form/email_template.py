5b2023382f4687b8edcd3768f09d44b22797a340
Allen Tuggle <allen.tuggle@excella.com>
2019-07-25 23:04:39 -0400

    awt: Added style and ability to manually insert html code into email template

--- events/form/email_template.py
+++ events/form/email_template.py
@@ -4,7 +4,7 @@ from sendemail.models.email_template import EmailTemplate
 
 class TemplateForm(forms.Form):
     subject = forms.CharField(label='Subject:', max_length=100, required=True)
-    body = forms.CharField(widget=forms.Textarea, required=True)
+    body = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 20}), required=True)
     enabled = forms.BooleanField(required=False)
 
     class Meta:
